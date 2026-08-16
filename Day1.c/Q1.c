f0117bf7fc29bea7ab0acd9d8735adc5a3659a61
sameer33694 <sameer.33694@stu.upes.ac.in>
2026-08-16 10:30:38 +0530

    Add files via upload

---
+++ Day1.c/Q1.c
@@ -0,0 +1,22 @@
+/*Q1: Write a program to input two numbers and display their sum.
+Sample Test Cases:
+Input 1:
+3 4
+Output 1:
+Sum = 7
+
+Input 2:
+-1 20
+Output 2:
+Sum = 19
+
+*/
+#include <stdio.h>
+
+void main(){
+    int a,b,c;
+    printf("Enter the values of a and b : ");
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    printf("Result=%d",c);
+}
