93197893b91aaf04ce6b028c221ec430ddac637a
sameer33694 <sameer.33694@stu.upes.ac.in>
2026-08-20 21:32:50 +0530

    Add files via upload

---
+++ Day11/Q22.c
@@ -0,0 +1,38 @@
+//Q22: Write a program to find profit or loss percentage given cost price and selling price.
+
+/*
+Sample Test Cases:
+Input 1:
+1000 1200
+Output 1:
+Profit 20%
+
+Input 2:
+1000 800
+Output 2:
+Loss 20%
+
+Input 3:
+1000 1000
+Output 3:
+No Profit No Loss
+
+*/
+#include <stdio.h>
+
+int main() {
+    double cp, sp;
+    scanf("%lf %lf", &cp, &sp);
+
+    if (sp > cp) {
+        double profitPercent = ((sp - cp) / cp) * 100;
+        printf("Profit %g%%\n", profitPercent);
+    } else if (sp < cp) {
+        double lossPercent = ((cp - sp) / cp) * 100;
+        printf("Loss %g%%\n", lossPercent);
+    } else {
+        printf("No Profit No Loss\n");
+    }
+
+    return 0;
+}
